5cc5dce0b89d2a665daee72f4727fdd464c99785
Omar Diab <me@omardiab.com>
2023-08-09 18:31:16 +0900

    revalidation test

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
-/** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+import Link from "next/link";
+
+export default function Home({searchParams}) {
+  const { value } = searchParams;
+  const randomNumber = Math.random();
+
+  return <div><h1>30s Revalidation Repro: Home</h1>
+  <h2>Value: {value}</h2>
+  <h2>Random number: {randomNumber}</h2>
+  <ul>
+    <li><Link href={{query: {value: 1}}}>Set Value to 1</Link></li>
+    <li><Link href={{query: {value: 2}}}>Set Value to 2</Link></li>
+  </ul>
+  </div>
 }
